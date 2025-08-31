1e0dfafb649e34994ef61b499015d734348aded9
killgrave000 <mahirlabib000@gmail.com>
2025-08-31 21:49:53 +0600

    chore: switch to Selenium Manager; remove webdriver-manager/ChromeType import

--- utils_browser.py
+++ utils_browser.py
@@ -1,18 +1,18 @@
 import os
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.utils import ChromeType
 
 def make_chromium_driver():
     opts = Options()
+    # Container/Render friendly flags
     opts.add_argument("--headless=new")
     opts.add_argument("--no-sandbox")
     opts.add_argument("--disable-dev-shm-usage")
     opts.add_argument("--disable-gpu")
     opts.add_argument("--window-size=1280,800")
+    # Point Selenium to the Chromium in the container
     opts.binary_location = os.environ.get("CHROME_BIN", "/usr/bin/chromium")
 
-    service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
-    return webdriver.Chrome(service=service, options=opts)
+    # Selenium Manager (built into Selenium 4.6+) will auto-download the right driver.
+    # No Service() and no webdriver-manager needed.
+    return webdriver.Chrome(options=opts)
